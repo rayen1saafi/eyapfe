a202c573c9a875e04e69e484c2e2ad28e562832f
Hazem-Farhati <79176361+Hazem-Farhati@users.noreply.github.com>
2024-05-09 12:51:10 +0100

    home second

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "../styles/home.css";
 import HomeHeader from "../Components/HomeHeader";
+import HomeFeatures from "../Components/HomeFeatures";
+import HomeAbout from "../Components/HomeAbout";
 const Home = () => {
   return (
     <>
       <HomeHeader />
+      <HomeFeatures/>
+      <HomeAbout/>
     </>
   );
 };
