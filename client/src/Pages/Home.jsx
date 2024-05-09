c8c4e50e8471dd1830f89d6969ac90ffadd22f08
Hazem-Farhati <79176361+Hazem-Farhati@users.noreply.github.com>
2024-05-09 20:38:13 +0100

    courses-home-card

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -3,12 +3,14 @@ import "../styles/home.css";
 import HomeHeader from "../Components/HomeHeader";
 import HomeFeatures from "../Components/HomeFeatures";
 import HomeAbout from "../Components/HomeAbout";
+import HomeCourses from "../Components/HomeCourses";
 const Home = () => {
   return (
     <>
       <HomeHeader />
       <HomeFeatures/>
       <HomeAbout/>
+      <HomeCourses/>
     </>
   );
 };
